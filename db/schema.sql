740b9f71153b80f3e58a2e972b4d24224d9cbdb0
jval <joshvaldez3320@gmail.com>
2019-05-08 21:48:01 -0600

    setup

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers_db
+(
+    id INT
+    AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR
+    (30) NOT NULL,
+devoured BOOLEAN,
+PRIMARY KEY
+    (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Bacon Avocado Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Chicken-Ranch Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Southwest Veggie Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('BBQ Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Blue Cheese Burger', true);
